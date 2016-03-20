722277c464740afe7a49be9a735caad6c762e648
kaustuv deolal <vutsuak96@gmail.com>
2016-03-20 15:17:36 +0530

    biotext in crude form

--- script_2.py
+++ script_2.py
@@ -22,34 +22,38 @@ def get_recent_links(tag, number=5):
         ct += 1
         if ct > 15:
             break
-    x= image_filter(codes_url)  # contains 2 lists having filtered urls having no click kik and push and owner's name
+    x = image_filter(codes_url)  # contains 2 lists having filtered urls having no click kik and push and owner's name
     print x[0]
-    print x[1] #user names who posted the pic printed
-    follows=[]
-    followers=[]
-    biotext=[]
+    print x[1]  # user names who posted the pic printed
+    follows = []
+    followers = []
+    biotext = []
     for i in x[1]:
-        i= "https://www.instagram.com/"+str(i)[1:-1]
-        resp=requests.get(i)
-        soup=bs4.BeautifulSoup(resp.content)
+        i = "https://www.instagram.com/" + str(i)[1:-1]
+        resp = requests.get(i)
+        soup = bs4.BeautifulSoup(resp.content)
         script = soup.find("script", text=re.compile('window\._sharedData'))
-        pat1=re.compile(r'"follows":{".+"')
-        pat2=re.compile(r'"biography":".+"')
-        biotext_temp=pat2.findall(script)
+        pat1 = re.compile(r'"follows":{".+"')
+        pat2 = re.compile(r'"biography":".+"')
+        biotext_temp = pat2.findall(script)
         try:
-             biotext.append(biotext_temp[0].encode('ascii','ignore').split("media")[0])
+            biotext.append(biotext_temp[0].encode('ascii', 'ignore').split("media")[0])
         except:
             pass
-        list_temp=pat1.findall(script)
+        list_temp = pat1.findall(script)
+
         followers.append(list_temp[0].split(",")[0])
         follows.append(list_temp[0].split(",")[2])
 
+    print biotext
     print follows
     print followers
+
+
 def image_filter(image_list):
-    final_urls=[]
+    final_urls = []
     urls = []
-    usernames=[]
+    usernames = []
     for i in image_list:
         try:
             resp = requests.get(i)
@@ -58,11 +62,11 @@ def image_filter(image_list):
             if "click" in x or "kik" in x or "push" in x:
                 continue
             else:
-                #print i
-                #print x
+                # print i
+                # print x
                 urls.append(i)
                 script = soup.find("script", text=re.compile('window\._sharedData'))
-                pattern=re.compile(r'"owner":{"username":".+""')
+                pattern = re.compile(r'"owner":{"username":".+""')
                 usernames.append(pattern.findall(script)[0].split(":")[2].split(",")[0])
 
         except:
